56cb4b796f34325b8bf37d5b3741e3a763e6eec4
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2025-04-13 20:59:26 -0300

    feat: start to implement query github on page

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,28 @@ import Background from "./components/background/Background"
 import Logo from "./components/Logo"
 import PerfilCard from "./components/PerfilCard"
 import SearchInput from "./components/SearchInput"
+import useQuery from "./hooks/useQuery"
+import { useState } from "react"
+import { fetchGitHubProfile } from "./services/queryGitHub"
 
 
 function App() {
+  const [searchProfile, setSearchProfile] = useState("")
+  const {error, isLoading, result} = useQuery(fetchGitHubProfile(searchProfile))
+
+  console.log(result)
 
   return (
     <Background>
       <Card>
         <div className="gap-[27px] w-full flex flex-col items-center">
           <Logo />
-          <SearchInput />
+          <SearchInput value={searchProfile} setValue={setSearchProfile}/>
+          <div className="text-white">
+            {searchProfile}
+          </div>
+          {isLoading && "...Carregando"}
+          <button className="text-white">clique</button>
           <PerfilCard></PerfilCard>
         </div>
       </Card>
--- src/hooks/useQuery.tsx
+++ src/hooks/useQuery.tsx
@@ -1,17 +1,20 @@
 import { useEffect, useState } from "react";
 
-export default function UseQuery<T>(query: Promise<T>){
+export default function useQuery<T>(query: Promise<T>){
     const [data, setData] = useState<T>()
     const [isLoading, setIsLoading] = useState(false)
     const [error, setError] = useState<Error>()
 
     useEffect(()=>{
         setIsLoading(true)
+        console.log("chegou aqui")
         query
             .then(res => setData(res))
             .catch(error => setError(error))
-            .finally(() => setIsLoading(false))
-    }, [query])
+            .finally(() => {
+                setIsLoading(false)}
+            )
+    }, [])
 
     return {
         result: data,
