723e2166ecaaf9915401f5f2089d4fc2308553ac
Guilherme <eduardomartinspiangers@gmail.com>
2025-04-14 19:18:46 +0000

    style: wrap logo

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function Card({children, className}: {children?: ReactNode; className?: string}){
     return (
-        <div className={`rounded-[25px] w-full items-center max-w-[804px] bg-[#D9D9D9] px-[33px] py-[18px] flex gap-8 ${className}`}>
+        <div className={`flex rounded-[25px] w-full items-center max-w-[804px] bg-[#D9D9D9] px-[33px] py-[18px] gap-8 ${className}`}>
             {children}
         </div>
     )
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ import gitHubText from '../assets/gitHubText.svg'
 
 export default function Logo(){
     return (
-        <div className="flex gap-[11px] h-[82px] items-center">
+        <div className="flex gap-[11px] min-h-[82px] items-center flex-wrap justify-center">
             <img className="w-[58px] h-[58px] object-contain" src={gitHub} alt="GitHub" />
             <span className="font-semibold text-6xl text-white">Perfil</span>
             <img className="h-[45px] w-40 object-contain" src={gitHubText} alt="GitHub Text" />
